49a8707b852e8c6baba84f77519f9ea622ed9657
sirisha2213 <siripuramsiri327@gmail.com>
2026-03-06 05:25:30 -0500

    Add files via upload

---
+++ day3/operators.py
@@ -0,0 +1,170 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a= 10
+b =20
+a+b
+30
+b-a
+10
+a*b
+200
+b/a
+2.0
+b//a
+2
+b%10
+0
+9%2
+1
+2**3
+8
+6**2
+36
+a=10
+b=20
+c=30
+a=a+10
+a
+20
+a=a+10
+a=a+10
+a
+40
+a+=10
+a
+50
+a-=10
+a
+40
+a/=10
+a
+4.0
+... a
+... 4.0
+... a=20
+... b=10
+... a==b
+... False
+... a!=b
+... True
+... a>b
+... True
+... a<b
+... False
+... a<=b
+... False
+... a>=b
+... True
+... a>10 and b>20
+... False
+... a>5 and b>5
+... True
+... a
+... 20
+... b
+... 10
+... a>10 or b>20
+... True
+... a%3==0
+... False
+... not a%3==0
+... True
+... #str list tuple dict set
+... s="python"
+... 'o' in s
+... True
+... 't' in s
+... True
+... 'p' in s
+... True
+... 'z' in s
+... False
+'k' not in s
+True
+'p' not in s
+False
+l=["swapnil",'saaketh','dileep','pushwanth','bharat']
+l
+['swapnil', 'saaketh', 'dileep', 'pushwanth', 'bharat']
+'dileep' in l
+True
+'ravi' in l
+False
+'saaketh' in l
+True
+'shirisha' not in l
+True
+t=(3,4,5,6,7)
+5 not in t
+False
+6 in t
+True
+s={"pending","done","todo"}
+"review" in s
+False
+"done" in s
+True
+d={"python":"s1","java":"s2","html":"s3"}
+"python" in s
+False
+"python" in d
+
+True
+"java" in d
+True
+"s1" in d
+False
+
+"s2" not in d
+True
+a=[1,2,3,4]
+b=[1,2,3,4]
+a==b
+True
+a is b
+False
+c=a
+c
+[1, 2, 3, 4]
+a==c
+True
+a is c
+True
+id(a)
+2695014004672
+id(b)
+2694968614912
+id(c)
+2695014004672
+c is not b
+True
+>>> 6 & 7
+6
+>>> 7  & 8
+0
+>>>  7 | 8
+...  
+SyntaxError: unexpected indent
+>>> 7|8
+15
+>>> 3 ^ 4
+7
+>>> ~12
+-13
+~18
+-19
+~0
+-1
+8<<1
+16
+16<<1
+32
+32<<1
+64
+64<<2
+256
+64
+64
+64>>2
+16
+5>>1
